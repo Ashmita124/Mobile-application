f327e389f40bbe81b54eabd2892cb853f79bf5f2
Ashmita124 <126133726+Ashmita124@users.noreply.github.com>
2024-11-18 22:53:11 +0545

    Add files via upload
    Basic Variables and Data Types

---
+++ Qn2.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = 'Alice';
+  int age = 25;
+  double height = 5.6;
+  bool isStudent = true;
+
+  print('Name: $name, Age: $age, Height: $height, Is Student: $isStudent');
+}
